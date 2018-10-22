cc1ffe9f7b652a7ddce0b7bbdae820ffc96a0f63
adnanbayuaji <adnan.bayuaji@gmail.com>
2018-10-22 09:55:53 +0700

    new_repo

---
+++ agent.php
@@ -0,0 +1,33 @@
+<?php
+class Transaction{
+// Connection instance
+private $connection;
+// table name
+private $table_name = "agent";
+// table columns
+public $id;
+public $idManager;
+public $agentName;
+public $phone;
+public $email;
+public $address;
+public $longitude;
+public $latitude;
+public $totalVehicle;
+public $rowStatus;
+public $createdDate;
+public $createdBy;
+public $updatedDate;
+public $updatedBy;
+public function __construct($connection){
+$this->connection = $connection;
+}
+//C
+public function create(){}
+//R
+public function read(){}
+//U
+public function update(){}
+//D
+public function delete(){}    
+}
